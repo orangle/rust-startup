4b5f9048ada6c0f4fa6068277c9d7440d3b99652
zhizhi.liu <zhizhi.liu@qunar.com>
2021-07-08 10:30:10 +0800

    ex: hello

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,18 +1,41 @@
 use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
+use structopt::StructOpt;
+use log::{info, warn};
 
-mod value_tour;
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
 
 fn main() {
-    println!("Hello, world!");
+    hello()
+}
 
-    let mut arr:Vec<i32> = vec![2, 3, 1];
-    arr.push(4);
+fn hello() {
+    env_logger::init();
+    info!("===================start=====================");
+    let args = Cli::from_args();
+    let resut = std::fs::read_to_string(&args.path);
+    let content = match resut {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("can not deal with {}, just exit here", error)
+        }
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            //???
+            println!("{}", line);
+        }
+    }
 
     let stdout = stdout();
     let out = b"Hello fellow Rustaceans!";
     let width = 24;
-
     let mut writer = BufWriter::new(stdout.lock());
     say(out, width, &mut writer).unwrap();
 }
